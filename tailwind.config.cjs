a426a0ad520fef2318e3ae3f892e3c83f3aed485
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-10-12 08:53:28 -0300

    chore: add design tokens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,35 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+
+      white: '#fff',
+      black: '#000',
+
+      gray: {
+        100: '#e1e1e6',
+        200: '#c4c4cc',
+        400: '#7c7c8a',
+        800: '#202024',
+        900: '#121224',
+      },
+
+      cyan: {
+        300: '#9be1fb',
+        500: '#81d8f7',
+      },
+    },
+
+    fontSize: {
+      xs: '0.875rem',
+      sm: '1rem',
+      md: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem',
+    },
+
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif',
