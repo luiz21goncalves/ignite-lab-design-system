42ecbb6edbd3e3fb6ddb686af36f9341b904f93c
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-10-12 21:17:18 -0300

    feat: add home page

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -7,20 +7,25 @@ export type HeadingProps = {
   size?: 'sm' | 'md' | 'lg';
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 };
 
 export function Heading(props: HeadingProps) {
-  const { size = 'md', asChild, children } = props;
+  const { size = 'md', asChild, children, className } = props;
 
   const Component = asChild ? Slot : 'h2';
 
   return (
     <Component
-      className={clsx('text-gray-100 font-bold font-sans', {
-        'text-lg': size === 'sm',
-        'text-xl': size === 'md',
-        'text-2xl': size === 'lg',
-      })}
+      className={clsx(
+        'text-gray-100 font-bold font-sans',
+        {
+          'text-lg': size === 'sm',
+          'text-xl': size === 'md',
+          'text-2xl': size === 'lg',
+        },
+        className,
+      )}
     >
       {children}
     </Component>
--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -7,20 +7,25 @@ export type TextProps = {
   size?: 'sm' | 'md' | 'lg';
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 };
 
 export function Text(props: TextProps) {
-  const { size = 'md', asChild, children } = props;
+  const { size = 'md', asChild, children, className } = props;
 
   const Component = asChild ? Slot : 'span';
 
   return (
     <Component
-      className={clsx('text-gray-100 font-sans', {
-        'text-xs': size === 'sm',
-        'text-sm': size === 'md',
-        'text-md': size === 'lg',
-      })}
+      className={clsx(
+        'text-gray-100 font-sans',
+        {
+          'text-xs': size === 'sm',
+          'text-sm': size === 'md',
+          'text-md': size === 'lg',
+        },
+        className,
+      )}
     >
       {children}
     </Component>
